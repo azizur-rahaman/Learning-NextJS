0c4f20c79ae4e502e671a5947bf0991a57238f3f
Azizur Rahaman <91342806+azizur-rahaman@users.noreply.github.com>
2024-12-23 16:00:59 +0600

    contact and about page complete

---
+++ src/app/blog/contact/page.tsx
@@ -0,0 +1,20 @@
+
+export default function ContactPage() {
+  return (
+    <>
+        <h1>Contact</h1>
+        <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+        <br />
+
+        <h1>Get in Touch</h1>
+
+        <ul>
+            <li>Email: <a href="contact@gmail.com">contact@gmail.com</a></li>
+            <li>Website: <a href="www.example.com">www.example.com</a></li>
+        </ul>
+
+    </>
+  );
+}
